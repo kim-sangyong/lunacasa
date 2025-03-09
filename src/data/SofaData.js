88bda90a4374c0d32bd11be0661362179235abda
kim-sangyong <sdragon0416@gmail.com>
2025-03-09 23:59:05 +0900

    최종 수정

--- src/data/SofaData.js
+++ src/data/SofaData.js
@@ -11,7 +11,7 @@ const SofaData = [
       name: "3인 패브릭 소파",
       img: FabricSofa,
       originalPrice: 350000,
-      salePrice: 310000,
+      salePrice: 311500,
     },
     {
       id: 2,
@@ -25,28 +25,28 @@ const SofaData = [
       name: "2인 극세사 소파",
       img: MicroFiberSofa,
       originalPrice: 220000,
-      salePrice: 190000,
+      salePrice: 191400,
     },
     {
       id: 4,
       name: "다인용 라운드 소파",
       img: RoundSofa,
       originalPrice: 270000,
-      salePrice: 230000,
+      salePrice: 232200,
     },
     {
       id: 5,
       name: "이태리 빈티지 소파",
       img: ItalianVintageSofa,
       originalPrice: 310000,
-      salePrice: 290000,
+      salePrice: 291400,
     },
     {
       id: 6,
       name: "프리미엄 빈티지 소파",
       img: PremiumVintageSofa,
       originalPrice: 450000,
-      salePrice: 400000,
+      salePrice: 400500,
     },
   ];
   
