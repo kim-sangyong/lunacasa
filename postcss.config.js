8de3a764339cc9ddd2a5a3862aec9a095af54943
kim-sangyong <sdragon0416@gmail.com>
2025-03-24 21:01:24 +0900

    최종 커밋

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-// postcss.config.js
-module.exports = {
-    plugins: [
-      require('autoprefixer')({
-        overrideBrowserslist: ['last 2 versions', 'ie 11'], // 호환할 브라우저 설정
-      })
-    ]
-  };
